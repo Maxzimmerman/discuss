53f70fbb6a40571650c37b845546198bb5acd1c6
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2025-05-28 14:32:28 +1000

    first commit

---
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,29 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  alias Discuss.Topic
+  alias Discuss.Repo
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, :index, topics: topics)
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Discuss.Topic{}, %{})
+    render(conn, :new, changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: ~p"/")
+      {:error, changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
+  end
+end
