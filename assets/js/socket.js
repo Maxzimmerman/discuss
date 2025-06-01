0430953ff4a966a33b0be8c5ab7d054ab96ae117
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2025-06-01 19:57:52 +1000

    finallized project

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -4,9 +4,55 @@ let socket = new Socket("/socket", {params: {token: window.userToken}});
 
 socket.connect();
 
-let channel = socket.channel("comments:1", {})
-channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp)})
-    .receive("error", resp => { console.log("Unable to join", resp)})
+const createSocket = (topicId) => {
+    let channel = socket.channel(`comments:${topicId}`, {})
 
-export default socket;
+    // join channel
+    channel.join()
+        .receive("ok", resp => { 
+            console.log(resp)
+            renderComments(resp.comments);
+        })
+        .receive("error", resp => { 
+            console.log("Unable to join", resp)
+        })
+
+    // looking for new comments in realtime
+    channel.on(`comments:${topicId}:new`, renderComment)
+
+    // add new comment
+    document.querySelector('#add-comment').addEventListener('click', () => {
+        const content = document.querySelector('textarea').value;
+        channel.push('comment:add', {content: content});
+    })
+}
+
+function renderComment(event) {
+    const renderedComment = commentTemplate(event.comment);
+    document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function renderComments(comments) {
+    const renderComments = comments.map(comment => {
+        return commentTemplate(comment);
+    });
+
+    document.querySelector('.collection').innerHTML = renderComments.join();
+}
+
+function commentTemplate(comment) {
+    let email = "Anonymous";
+    if(comment.user) {
+        email = comment.user.email;
+    }
+    return `
+            <li class="collection-item">
+                ${comment.content}
+                <div class="secondary-content">
+                    ${email}
+                </div>
+            </li>
+        `;
+}
+
+window.createSocket = createSocket;
--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,13 +1,41 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
+  alias Discuss.{ Topic, Repo, Comment}
 
-  def join(name, _params, socket) do
-    IO.puts("++++++++")
-    IO.puts(name)
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    # query the topic with all belonging comments
+    topic_id = String.to_integer(topic_id)
+    # we get all users of comments that belong to the current topic
+    topic = Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(comments: [:user])
+
+    # return the comments and add the topic to the socket to the handle method can work with it
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
-  def handle_in() do
+  def handle_in(name, %{"content" => content}, socket) do
+    # add a new comment to the current topic
+    topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
 
+    changeset = topic
+      |> Ecto.build_assoc(:comments, user_id: user_id)
+      |> Comment.changeset(%{content: content})
+
+    # insert new comment for current topic
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        # the broadcast method send the data to all subscribers so
+        # the new socket is displayed in real time for all subscribers
+        broadcast!(
+          socket,
+          "comments:#{socket.assigns.topic.id}:new",
+          %{comment: comment})
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+    {:reply, :ok, socket}
   end
 end
